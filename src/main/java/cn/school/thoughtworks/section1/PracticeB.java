74b763aeca1c79a8bf9781b9439fb0089bf6a909
shuhan2 <2472520650@qq.com>
2018-05-08 20:59:52 +0800

    1-B

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -2,9 +2,12 @@ package cn.school.thoughtworks.section1;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中与2集合中子数组的元素相同的元素
+        return collection1.stream().filter((item) ->{for(List<String> list:collection2){ return  list.contains(item)==true;} return false;}).collect(toList());
     }
 }
