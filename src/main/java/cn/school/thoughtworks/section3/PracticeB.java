5772d3d46ea5eb4044a3e762bedb6ef2a30b610c
shuhan2 <2472520650@qq.com>
2018-05-08 21:06:16 +0800

    1-B

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,26 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+//选出1集合中元素的key的值跟2对象中value属性中的元素相同的元素,把他们的count-1，输出减过之后的新集合
+        Map<String,Integer> result = new HashMap<String,Integer>(collectionA);
+        List<String> list2 = new ArrayList();
+        int diff = 0;               //满3减1
+        for (String string :object.get("value")){
+            list2.add(string);
+        }
+        for (String str :list2){
+            if (result.containsKey(str)){
+                diff = result.get(str)/3;
+                result.put(str,result.get(str)-diff);
+            }
+        }
+        return result;
     }
 }
